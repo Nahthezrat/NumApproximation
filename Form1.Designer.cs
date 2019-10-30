1f48ee6a81f1d0a08ab9654d3eb6f8cf9dce84ba
NahtHezrat <nahthezrat@gmail.com>
2019-10-30 11:23:31 -0600

    Linear Approximation (v1)

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,33 +28,38 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series5 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            System.Windows.Forms.DataVisualization.Charting.Series series6 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea5 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend5 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series9 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series10 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
             // 
             // chart1
             // 
-            chartArea3.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea3);
-            legend3.Name = "Legend1";
-            this.chart1.Legends.Add(legend3);
+            chartArea5.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea5);
+            legend5.Name = "Legend1";
+            this.chart1.Legends.Add(legend5);
             this.chart1.Location = new System.Drawing.Point(219, 8);
             this.chart1.Name = "chart1";
-            series5.ChartArea = "ChartArea1";
-            series5.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
-            series5.Legend = "Legend1";
-            series5.Name = "Spline";
-            series6.ChartArea = "ChartArea1";
-            series6.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
-            series6.Legend = "Legend1";
-            series6.Name = "Nodes";
-            this.chart1.Series.Add(series5);
-            this.chart1.Series.Add(series6);
+            series9.ChartArea = "ChartArea1";
+            series9.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series9.Legend = "Legend1";
+            series9.Name = "Spline";
+            series10.ChartArea = "ChartArea1";
+            series10.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series10.Legend = "Legend1";
+            series10.Name = "Nodes";
+            this.chart1.Series.Add(series9);
+            this.chart1.Series.Add(series10);
             this.chart1.Size = new System.Drawing.Size(753, 500);
             this.chart1.TabIndex = 0;
             this.chart1.Text = "chart1";
@@ -70,17 +75,69 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.Button1_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(25, 182);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 31);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "y=";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(100, 182);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 31);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "e";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(63, 183);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(37, 35);
+            this.textBox1.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(166, 172);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(24, 29);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "x";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox2.Location = new System.Drawing.Point(132, 172);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(33, 32);
+            this.textBox2.TabIndex = 6;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(984, 520);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.chart1);
             this.Name = "Form1";
             this.Text = "Approximation";
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -88,6 +145,11 @@
 
         private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -19,9 +19,257 @@ namespace NumApproximation
         {
             InitializeComponent();
         }
+    static double Income(double x1, double x2, double x3, double[] coef)
+    {
+      // x1 = education, x2 = work, x3 = sex
+      double result; // the constant
+      result = coef[0] + (x1 * coef[1]) + (x2 * coef[2]) + (x3 * coef[3]);
+      return result;
+    }
+
+    static double RSquared(double[][] data, double[] coef)
+    {
+      // 'coefficient of determination'
+      int rows = data.Length;
+      int cols = data[0].Length;
+
+      // 1. compute mean of y
+      double ySum = 0.0;
+      for (int i = 0; i < rows; ++i)
+        ySum += data[i][cols - 1]; // last column
+      double yMean = ySum / rows;
+ 
+      // 2. sum of squared residuals & tot sum squares
+      double ssr = 0.0;
+      double sst = 0.0;
+      double y; // actual y value
+      double predictedY; // using the coef[] 
+      for (int i = 0; i < rows; ++i)
+      {
+        y = data[i][cols - 1]; // get actual y
+
+        predictedY = coef[0]; // start w/ intercept constant
+        for (int j = 0; j < cols - 1; ++j) // j is col of data
+          predictedY += coef[j+1] * data[i][j]; // careful
+        
+        ssr += (y - predictedY) * (y - predictedY);
+        sst += (y - yMean) * (y - yMean);
+      }
+
+      if (sst == 0.0)
+        throw new Exception("All y values equal");
+      else
+        return 1.0 - (ssr / sst);
+     }
+
+    static double[][] DummyData(int rows, int seed)
+    {
+      // generate dummy data for linear regression problem
+      double b0 = 15.0;
+      double b1 = 0.8; // education years
+      double b2 = 0.5; // work years
+      double b3 = -3.0; // sex = 0 male, 1 female
+      Random rnd = new Random(seed);
+
+      double[][] result = new double[rows][];
+      for (int i = 0; i < rows; ++i)
+        result[i] = new double[4];
+
+      for (int i = 0; i < rows; ++i)
+      {
+        int ed = rnd.Next(12, 17); // 12, 16]
+        int work = rnd.Next(10, 31); // [10, 30]
+        int sex = rnd.Next(0, 2); // 0 or 1
+        double y = b0 + (b1 * ed) + (b2 * work) + (b3 * sex);
+        y += 10.0 * rnd.NextDouble() - 5.0; // random [-5 +5]
+
+        result[i][0] = ed;
+        result[i][1] = work;
+        result[i][2] = sex;
+        result[i][3] = y; // income
+      }
+      return result;
+    }
+
+    static double[][] Design(double[][] data)
+    {
+      // add a leading col of 1.0 values
+      int rows = data.Length;
+      int cols = data[0].Length;
+      double[][] result = MatrixCreate(rows, cols + 1);
+      for (int i = 0; i < rows; ++i)
+        result[i][0] = 1.0;
+
+      for (int i = 0; i < rows; ++i)
+        for (int j = 0; j < cols; ++j)
+          result[i][j + 1] = data[i][j];
+
+      return result;
+    }
+
+    static double[] Solve(double[][] design)
+    {
+      // find linear regression coefficients
+      // 1. peel off X matrix and Y vector
+      int rows = design.Length;
+      int cols = design[0].Length;
+      double[][] X = MatrixCreate(rows, cols - 1);
+      double[][] Y = MatrixCreate(rows, 1); // a column vector
+
+      int j;
+      for (int i = 0; i < rows; ++i)
+      {
+        for (j = 0; j < cols - 1; ++j)
+        {
+          X[i][j] = design[i][j];
+        }
+        Y[i][0] = design[i][j]; // last column
+      }
+
+      // 2. B = inv(Xt * X) * Xt * y
+      double[][] Xt = MatrixTranspose(X);
+      double[][] XtX = MatrixProduct(Xt, X);
+      double[][] inv = MatrixInverse(XtX);
+      double[][] invXt = MatrixProduct(inv, Xt);
+
+      double[][] mResult = MatrixProduct(invXt, Y);
+      double[] result = MatrixToVector(mResult);
+      return result;
+    } // Solve
+
+
+    static void ShowMatrix(double[][] m, int dec)
+    {
+      for (int i = 0; i < m.Length; ++i)
+      {
+        for (int j = 0; j < m[i].Length; ++j)
+        {
+          Console.Write(m[i][j].ToString("F" + dec) + "  ");
+        }
+        Console.WriteLine("");
+      }
+    }
+
+    static void ShowVector(double[] v, int dec)
+    {
+      for (int i = 0; i < v.Length; ++i)
+        Console.Write(v[i].ToString("F" + dec) + "  ");
+      Console.WriteLine("");
+    }
 
         private void Button1_Click(object sender, EventArgs e)
         {
+            chart1.Series[0].Points.Clear();
+            chart1.Series[1].Points.Clear();
+
+            int rows = 6;
+            double[][] data = new double[rows][];
+            double[,] MatrixA = new double[2, 2];
+            MatrixA[0, 0] = rows;
+            MatrixA[0, 1] = 0;
+            MatrixA[1, 0] = 0;
+            MatrixA[1, 1] = 0;
+            double[] VectorB = new double[2];
+            VectorB[0] = 0;
+            VectorB[1] = 0;
+
+            using (StreamReader sstream = new StreamReader("nodes.in"))
+            {
+                string line;
+                for(int i = 0; i < rows; ++i)
+                {
+                    line = sstream.ReadLine();
+                    Console.WriteLine("Node " + i + ": " + line);
+
+                    data[i] = new double[2];
+                    string[] tokens = line.Split();
+                    data[i][0] = double.Parse(tokens[0]);
+                    data[i][1] = double.Parse(tokens[1]);
+
+                    //Заполнение матрицы A
+                    MatrixA[0, 1] += double.Parse(tokens[0]);
+                    MatrixA[1, 0] += double.Parse(tokens[0]);
+                    MatrixA[1, 1] += double.Parse(tokens[0]) * double.Parse(tokens[0]);
+                    Console.WriteLine(MatrixA[0, 1]);
+                    Console.WriteLine(MatrixA[1, 0]);
+                    Console.WriteLine(MatrixA[1, 1]);
+                   //Заполнение матрицы B
+                    VectorB[0] += double.Parse(tokens[1]);
+                    VectorB[1] += double.Parse(tokens[1]) * double.Parse(tokens[0]);
+                    Console.WriteLine(VectorB[0]);
+                    Console.WriteLine(VectorB[1]);
+
+                    chart1.Series[1].Points.AddXY(double.Parse(tokens[0]), double.Parse(tokens[1]));
+                }
+            }
+
+            
+            
+            
+            LinearSystem system = new LinearSystem(MatrixA, VectorB);
+            Console.WriteLine(system.XVector[0]);
+            Console.WriteLine(system.XVector[1]);
+            for (double x = 0.4; x <= 2.4; x += 0.01)
+            {
+                chart1.Series[0].Points.AddXY(x, system.XVector[0] + system.XVector[1]*x);
+            }
+
+            /*
+            Console.WriteLine("Creating " + rows + " rows synthetic data");
+
+            //double[][] data = MatrixLoad("..\\..\\IncomeData.txt", true, ',');
+
+            Console.WriteLine("Education-Work-Sex-Income data:\n");
+            ShowMatrix(data, 2);
+
+            
+            Console.WriteLine("\nCreating design matrix from data");
+            double[][] design = Design(data); // 'design matrix'
+            Console.WriteLine("Done\n");
+
+            Console.WriteLine("Design matrix:\n");
+            ShowMatrix(design, 2);
+
+            Console.WriteLine("\nFinding coefficients using inversion");
+            double[] coef = Solve(design); // use design matrix
+            Console.WriteLine("Done\n");
+
+            Console.WriteLine("Coefficients are:\n");
+            ShowVector(coef, 4);
+            Console.WriteLine("");
+
+            Console.WriteLine("Coefficients:\n");
+            Console.WriteLine(coef[0]);
+            Console.WriteLine(coef[1]);
+
+            for (double x = 0.4; x <= 2.4; x += 0.01)
+            {
+                chart1.Series[0].Points.AddXY(x, Math.Exp(coef[0] + coef[1]*x));
+            }
+            */
+            /*
+            Console.WriteLine("Computing R-squared\n");
+            double R2 = RSquared(data, coef); // use initial data
+            Console.WriteLine("R-squared = " + R2.ToString("F4"));
+
+            Console.WriteLine("\nPredicting income for ");
+            Console.WriteLine("Education = 14");
+            Console.WriteLine("Work      = 12");
+            Console.WriteLine("Sex       = 0 (male)");
+
+            double y = Income(14, 12, 0, coef);
+            Console.WriteLine("\nPredicted income = " + y.ToString("F2"));
+
+            Console.WriteLine("\nEnd linear regression demo\n");
+            Console.ReadLine();
+            */
+
+            /*
+    
+            chart1.Series[0].Points.Clear();
+            chart1.Series[1].Points.Clear();
+
+           
             int nodesNum = 0;
             using (StreamReader sstream = new StreamReader("nodes.in"))
             {
@@ -37,6 +285,7 @@ namespace NumApproximation
                     chart1.Series[1].Points.AddXY(double.Parse(tokens[0]), double.Parse(tokens[1]));
                 }
             }
+            */
         }
     }
 }
